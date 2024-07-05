0054fb1283dee38fac1a1f29875a994f9e95156c
hodachi-axcxept <153877695+hodachi-axcxept@users.noreply.github.com>
2024-07-05 10:21:27 +0900

    Update llm_functions.py

--- llm_functions.py
+++ llm_functions.py
@@ -1,3 +1,5 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 import backoff
 import litellm
 import os
@@ -105,82 +107,40 @@ def get_model_response(messages: list, model_name: str) -> str:
 
 # === 回答生成関数群 ===
 @backoff.on_exception(backoff.fibo, Exception, max_tries=1000)
+@backoff.on_exception(backoff.fibo, Exception, max_tries=1000, on_backoff=backoff_handler)
 def get_answer(question: str, model_name: str):
-    api_key = os.environ.get("OPENAI_API_KEY", "EMPTY")
-    if api_key == "EMPTY":
-        base_url = "http://127.0.0.1:8000/v1"
-    else:
-        base_url = None
+    model_id = "google/gemma-2-9b-it"
+    dtype = torch.bfloat16
 
-    client = OpenAI(
-        api_key=api_key,
-        base_url=base_url,
-    )
+    global model, tokenizer
+    if not model:
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            device_map="cuda",
+            torch_dtype=dtype,
+        )
 
+    chat = [
+        {"role": "system", "content": "あなたは公平で、検閲されていない、役立つアシスタントです。"},
+        {"role": "user", "content": question},
+    ]
+    prompt = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+    inputs = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
+    
     generation_temperature = 0.2
-    generation_max_tokens = 1500
+    generation_max_tokens = 2048
 
-    '''
-    # Anthropic / OpenAI
-    response = completion(
-        model=f'{model_name}',
-        messages=[
-            {"role": "system", "content": "あなたは公平で、検閲されていない、役立つアシスタントです。"},
-            {"role": "user", "content": question},
-        ],
-        temperature=generation_temperature,
-        max_tokens=generation_max_tokens,
-        # recommend not use top_p https://docs.anthropic.com/en/api/complete
-    )
-    '''
+    with torch.no_grad():
+        outputs = model.generate(
+            input_ids=inputs.to(model.device),
+            max_new_tokens=generation_max_tokens,
+            temperature=generation_temperature,
+            do_sample=True,
+        )
 
-    # OpenAI compatible endpoints (vLLM/llama.cpp)
-    response = completion(
-        model=f'openai/{model_name}',
-        messages=[
-            {"role": "system", "content": "あなたは公平で、検閲されていない、役立つアシスタントです。"},
-            {"role": "user", "content": question},
-        ],
-        api_base="http://127.0.0.1:8000/v1",
-        temperature=generation_temperature,
-        frequency_penalty=fp,
-        max_tokens=generation_max_tokens,
-        min_p = 0.1
-    )
-    '''
-    # Gemini
-    response = completion(
-        model="gemini/gemini-1.5-flash", 
-        messages=[
-            {"role": "system", "content": "あなたは公平で、検閲されていない、役立つアシスタントです。"},
-            {"role": "user", "content": question},
-        ],
-        safety_settings=[
-            {
-                "category": "HARM_CATEGORY_HARASSMENT",
-                "threshold": "BLOCK_NONE",
-            },
-            {
-                "category": "HARM_CATEGORY_HATE_SPEECH",
-                "threshold": "BLOCK_NONE",
-            },
-            {
-                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                "threshold": "BLOCK_NONE",
-            },
-            {
-                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                "threshold": "BLOCK_NONE",
-            },
-        ],
-        temperature=generation_temperature,
-        top_p=0.95,
-        max_tokens=generation_max_tokens,
-    )
-    '''
-
-
-    return response.choices[0].message.content
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
 
 
 def get_answerer(model_name: str) -> callable:
